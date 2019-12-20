44d3bde2efb66b3238da1b40a8463e2e9fe28baa
alsvsl <alisa.vasilewa@yandex.ru>
2019-12-21 01:05:29 +0300

    changed so much that even don't remember.....

--- jcQueries.py
+++ jcQueries.py
@@ -2,6 +2,8 @@ import psycopg2
 import datetime
 from jcUserClass import User
 from psycopg2 import sql
+from jcSupplicantClass import Supplicant
+from jcCaseClass import Case
 
 
 class DataBase:
@@ -86,7 +88,7 @@ class DataBase:
         else:
             return None
 
-    def get_order_by_points(self):
+    def get_students_order_by_points(self):
         conn = psycopg2.connect(
             dbname=self.name, user=self.user, password=self.password, host=self.host)
         records = []
@@ -251,3 +253,145 @@ class DataBase:
             return error
         finally:
             conn.close()
+
+    def get_case_by_id(self, id):
+        conn = psycopg2.connect(
+            dbname=self.name, user=self.user, password=self.password, host=self.host)
+        with conn.cursor() as cursor:
+            conn.autocommit = True
+            cursor.execute('SELECT * FROM cases WHERE case_id = %s', (id, ))
+            records = cursor.fetchall()
+        conn.close()
+        if len(records) > 0:
+            case = Case(records[0])
+            return case
+        else:
+            return None
+
+    def update_case_by_id(self, case):
+        if self.get_case_by_id(case.case_id) == None:
+            return None
+        case_id = case.case_id
+        conn = psycopg2.connect(
+            dbname=self.name, user=self.user, password=self.password, host=self.host)
+        with conn.cursor() as cursor:
+            now = datetime.datetime.now()
+            sql = """UPDATE cases
+                        SET category = %s,
+                        title = %s,
+                        description = %s,
+                        s_id =  %s,
+                        t_id =  %s,
+                        status =  %s,
+                        supplicant_id = %s,
+                        dispatcher_id = %s,
+                        last_update = %s
+                        WHERE case_id = %s"""
+            try:
+                # execute the UPDATE  statement
+                cursor.execute(sql, (case.category, case.title, case.description, case.s_id,
+                                     case.t_id, case.status, case.supplicant_id, case.dispatcher_id, str(
+                                         now),
+                                     case_id))
+                # get the number of updated rows
+                conn.commit()
+                # Close communication with the PostgreSQL database
+                cursor.close()
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+
+    def insert_supplicant(self, supplicants):
+        conn = psycopg2.connect(
+            dbname=self.name, user=self.user, password=self.password, host=self.host)
+        try:
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                values = []
+                for s in supplicants:
+                    values.append(
+                        (s.name, s.telephone_number))
+                insert = sql.SQL('INSERT INTO supplicants(name, telephone_number) VALUES {}').format(
+                    sql.SQL(',').join(map(sql.Literal, values))
+                )
+                cursor.execute(insert)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            return error
+        finally:
+            conn.close()
+
+    def get_supplicant_by_id(self, id):
+        conn = psycopg2.connect(
+            dbname=self.name, user=self.user, password=self.password, host=self.host)
+        with conn.cursor() as cursor:
+            conn.autocommit = True
+            cursor.execute(
+                'SELECT * FROM supplicants WHERE supplicant_id = %s', (id, ))
+            records = cursor.fetchall()
+        conn.close()
+        if len(records) > 0:
+            supplicant = Supplicant(records[0])
+            return supplicant
+        else:
+            return None
+
+    def delete_supplicant_by_id(self, id):
+        if self.get_supplicant_by_id(id) == None:
+            return None
+        conn = psycopg2.connect(
+            dbname=self.name, user=self.user, password=self.password, host=self.host)
+        with conn.cursor() as cursor:
+            conn.autocommit = True
+            cursor.execute(
+                'DELETE FROM supplicants WHERE supplicant_id = %s', (id, ))
+        conn.close()
+        return id
+
+    def get_cases_by_student_id(self, student_id):
+        conn = psycopg2.connect(
+            dbname=self.name, user=self.user, password=self.password, host=self.host)
+        records = []
+        with conn.cursor() as cursor:
+            conn.autocommit = True
+            cursor.execute(
+                'SELECT * FROM cases WHERE s_id = %s', (student_id, ))
+            records = cursor.fetchall()
+        conn.close()
+        cases = []
+        if len(records) > 0:
+            for rec in records:
+                cs = Case(rec)
+                cases.append(cs)
+            return cases
+        else:
+            return None
+
+    def get_cases_by_teacher_id(self, teacher_id):
+        conn = psycopg2.connect(
+            dbname=self.name, user=self.user, password=self.password, host=self.host)
+        records = []
+        with conn.cursor() as cursor:
+            conn.autocommit = True
+            cursor.execute(
+                'SELECT * FROM cases WHERE t_id = %s', (teacher_id, ))
+            records = cursor.fetchall()
+        conn.close()
+        cases = []
+        if len(records) > 0:
+            for rec in records:
+                cs = Case(rec)
+                cases.append(cs)
+            return cases
+        else:
+            return None
+
+
+db = DataBase()
+
+st = db.get_cases_by_student_id(6)
+t = db.get_cases_by_teacher_id(2)
+for s in st:
+    print(s.title)
+
+for tt in t:
+    print(tt.title)
