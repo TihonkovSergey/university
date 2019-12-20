fd55d3144bf43ed7c6d1c31d1fa0c4bfabed5cde
TihonkovSergey <slezki.edinoroga@mail.ru>
2019-12-20 23:11:35 +0300

    add new functional and bug fix

--- teacher_window.py
+++ teacher_window.py
@@ -3,47 +3,40 @@ from jcQueries import DataBase
 import windows_init
 
 def show_teacher(main_user):
-    def show_users(event, users):
-        lbox.delete(0,tk.END)
-        for i in users:
-            lbox.insert(0,i.name)
-    def show_selected(event, users):
+    """def show_selected(event, users):
         label['text'] = ""
         select = list(lbox.curselection())
         if len(select):
-            label['text'] = users[-select[0] -1].name
-    def leave_akk(event, main_user):
+            label['text'] = users[ select[0] ].name"""
+    def leave_akk():
         root.destroy()
         windows_init.show_login(main_user)
     def show_students():
         root.destroy()
         windows_init.show_my_students(main_user)
+    def show_my_cases():
+        root.destroy()
+        windows_init.show_my_cases(main_user)
     
     root = tk.Tk()
     root.resizable(False, False)
     root.title("Авторизация")
     screen_width = root.winfo_screenwidth() // 2 - 320 
     screen_height = root.winfo_screenheight() // 2 - 210 
-    root.geometry('840x620+{}+{}'.format(screen_width, screen_height))
+    root.geometry('640x420+{}+{}'.format(screen_width, screen_height))
 
     db = DataBase()
-    b_show = tk.Button(text="Показать", compound=tk.TOP)
-    b_get = tk.Button(text="Вывести выбранного")
-    b_login = tk.Button(text="Выйти из аккаунта")
-    b_show_my_students  = tk.Button(text="Мои студенты", command=show_students)
-    label = tk.Label(bg='black', fg='white', width=40)
-    lbox = tk.Listbox(width = 40, height = 10) 
-    
-    users_list = db.get_all_users()
+    db.name = "postgres"      #TODO: delete this
+    db.password = "postgres"  #TODO: delete this
 
-    b_show.bind('<Button-1>', lambda event, users=users_list: show_users(event, users))
-    b_get.bind('<Button-1>', lambda event, users=users_list: show_selected(event, users))
-    b_login.bind('<Button-1>', lambda event, users=main_user: leave_akk(event, main_user))
+    b_show_my_students  = tk.Button(text="Мои студенты", command=show_students)
+    b_im = tk.Button(text="Мой профиль") #TODO: сделать переход в Мой профиль
+    b_show_my_cases = tk.Button(text="Мои дела", command=show_my_cases)
+    b_login = tk.Button(text="Выйти из аккаунта", command=leave_akk)
+    
+    b_im.pack(side="top")
+    b_show_my_cases.pack(side="top")
     b_show_my_students.pack(side="top")
-    b_show.pack(side=tk.LEFT)
-    b_get.pack(side=tk.LEFT)
-    label.pack(side=tk.LEFT)
-    lbox.pack(side=tk.LEFT)
     b_login.pack(side=tk.RIGHT)
     
     root.mainloop()
