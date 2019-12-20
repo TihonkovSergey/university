6573d671d97b32bfe4d6ef6436cc34eb3525de93
TihonkovSergey <slezki.edinoroga@mail.ru>
2019-12-21 00:47:24 +0300

    clearing main

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import tkinter as tk
 import windows_init
-from jcQueries import DataBase
-from tkinter import ttk
-from tkinter import filedialog as fd
 from jcUserClass import User
-
 
 if __name__ == "__main__":
     main_user = User(("id", "name", "type", "comp", "login", "password", "pd", "0.0"))
-    
     windows_init.show_login(main_user)
-
-    """
-    root = tk.Tk()
-    log_win = LoginWindow(root, main_user)
-    next_win = TeacherWindow(log_win, main_user)
-    log_win.pack()
-    root.title("")
-    root.geometry("650x450+300+200")
-    root.resizable(False, False)
-    root.mainloop()"""
