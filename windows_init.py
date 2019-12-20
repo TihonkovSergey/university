308f2c120f94fd15285d0333e1a76e4d3fc80147
TihonkovSergey <slezki.edinoroga@mail.ru>
2019-12-20 23:12:24 +0300

    update initialisation

--- windows_init.py
+++ windows_init.py
@@ -9,5 +9,8 @@ def show_login(main_user):
 def show_teacher(main_user):
     teacher_window.show_teacher(main_user)
 
+def show_my_cases(main_user):
+    my_cases_window.show_my_cases(main_user)
+
 def show_my_students(main_user):
     my_students_window.show_my_students(main_user)
