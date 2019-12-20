c7722f7e854892d0212f1765ea2b4de1bc730e96
TihonkovSergey <slezki.edinoroga@mail.ru>
2019-12-20 18:28:09 +0300

    Added field points for User

--- jcUserClass.py
+++ jcUserClass.py
@@ -1,6 +1,5 @@
 import json
 
-
 class User:
     def __init__(self, user):
         self.id = str(user[0])
@@ -10,6 +9,7 @@ class User:
         self.login = str(user[4])
         self.password = str(user[5])
         self.personal_data = str(user[6])
+        self.points = float(user[7])
 
     def __str__(self):
         return (self.id + " " +
@@ -18,7 +18,8 @@ class User:
                 self.competence + " " +
                 self.login + " " +
                 self.password + " " +
-                self.personal_data)
+                self.personal_data + " " + 
+                str(self.points))
 
     def __print__(self):
         print(str(self))
@@ -30,4 +31,5 @@ class User:
                 self.competence == other.competence and
                 self.login == other.login and
                 self.password == other.password and
-                self.personal_data == other.personal_data)
+                self.personal_data == other.personal_data and
+                str(self.points) == str(other.points))
