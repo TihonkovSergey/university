33773a48cb5a1267838e0842704926a9840c9776
alsvsl <alisa.vasilewa@yandex.ru>
2019-12-20 19:13:19 +0300

    supplicant class

---
+++ jcSupplicantClass.py
@@ -0,0 +1,13 @@
+class Supplicant:
+    def __init__(self, supplicant):
+        self.supplicant_id = str(supplicant[0])
+        self.name = str(supplicant[1])
+        self.telephone_number = str(supplicant[2])
+
+    def __str__(self):
+        return (self.supplicant_id + " " +
+                self.name + " " +
+                self.telephone_number)
+
+    def __print__(self):
+        print(str(self))
