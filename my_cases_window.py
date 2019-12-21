d85ff6ae7356c019fdda36f50231775b2d2b1283
TihonkovSergey <slezki.edinoroga@mail.ru>
2019-12-21 04:16:28 +0300

    add link on case window

--- my_cases_window.py
+++ my_cases_window.py
@@ -17,27 +17,50 @@ def show_my_cases(main_user):
         lbox.delete(0,tk.END)
         global curr_cases
         if main_user.type == "тьютор":
-            curr_cases = db.get_all_cases() # правильно?
+            curr_cases = db.get_all_cases()
         elif main_user.type == "преподаватель":
             curr_cases = db.get_cases_by_teacher_id(main_user.id)
         elif main_user.type == "студент": 
             curr_cases = db.get_cases_by_student_id(main_user.id)
         if not curr_cases:
+            lbox.insert(tk.END, "Ничего не найдено")
             curr_cases = []
         for c in curr_cases:
             lbox.insert(0,c.title)
     def show_check():
         lbox.delete(0,tk.END)
         global curr_cases
-        if main_user.type == "преподаватель":
+        if main_user.type == "тьютор":
             pass
-            #cases = get_cases_by_cat_for_id() # ???
+            #cases = get_cases_by_cat_for_id() # TODO: требующие добавления ответственных
+        elif main_user.type == "преподаватель":
+            curr_cases = db.get_cases_which_need_teacher_editing(main_user.id)
         else:
-            pass # ???
-    def show_finished():
+            curr_cases = db.get_cases_which_need_student_editing(main_user.id)
+        if not curr_cases:
+            lbox.insert(tk.END, "Ничего не найдено")
+            curr_cases = []
+        for c in curr_cases:
+            lbox.insert(0,c.title)
+    def show_finished(): # TODO: добавить функции запросов когда появятся  
         lbox.delete(0,tk.END)
         global curr_cases
 
+        if not curr_cases:
+            lbox.insert(tk.END, "Ничего не найдено")
+            curr_cases = []
+        for c in curr_cases:
+            lbox.insert(0,c.title)
+
+    def show_case_win():
+        global curr_cases
+        select = list(lbox.curselection())
+        if len(select) and curr_cases:
+            select_case = curr_cases[select[0]]
+            root.destroy()
+            windows_init.show_case_window(main_user, select_case)
+        else:
+            label['text'] = "Выберите дело!"
     root = tk.Tk()
     root.resizable(False, False)
     root.title("Мои дела")
@@ -56,11 +79,15 @@ def show_my_cases(main_user):
     b_finished_cases = tk.Button(text="Завершенные дела", command=show_finished)
     b_back = tk.Button(text="Назад", command=go_back)
     lbox = tk.Listbox(width = 40, height = 10)
+    b_case_win = tk.Button(text="Просмотреть выбранное дело", command=show_case_win)
+    label = tk.Label(width=40)
 
     b_all_cases.pack()
     b_check_cases.pack()
     b_finished_cases.pack()
     lbox.pack()
+    b_case_win.pack()
+    label.pack()
     b_back.pack(side=tk.RIGHT)
 
     root.mainloop()
