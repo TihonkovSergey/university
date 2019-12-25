c8db1fc28c9a39020dc93f2b917b8d0090e72742
TihonkovSergey <slezki.edinoroga@mail.ru>
2019-12-25 17:52:35 +0300

    add functional for admin and input checker

--- profile_window.py
+++ profile_window.py
@@ -1,10 +1,42 @@
 import tkinter as tk
 from jcQueries import DataBase
+from tkinter import messagebox as mb
 import windows_init
 
 def show_profile(main_user, user):
     def save():
-        user.name = e_name.get()
+        name = e_name.get()
+        if len(name) < 6:
+            mb.showerror("Ошибка", "Имя должно быть длиннее 6 символов!")
+            return
+        for c in name:
+            if not (("a" <= c <= "z") or ("A" <= c <= "Z") or 
+                    ("а" <= c <= "я") or ("А"<= c <= "Я") or c==" "):
+                mb.showerror("Ошибка", "Имя должно содержать только буквы!")
+                return
+
+        login = e_login.get()
+
+        if len(login) < 4:
+            mb.showerror("Ошибка", "Логин должен быть длиннее 4 символов!")
+            return
+        
+        for c in login:
+            if not (("a" <= c <= "z") or ("A" <= c <= "Z") or ("0" <= c <= "9")):
+                mb.showerror("Ошибка", "Логин должен содержать только латинские буквы и цифры!")
+                return
+
+        user.name = name
+        
+        select_comp = list(lb_comp.curselection())
+        if len(select_comp):
+            if user.type == "студент":
+                user.competence = s_competence_list[select_comp[0]]
+            else:
+                user.competence = t_competence_list[select_comp[0]]
+        
+        user.login = login
+
         db.update_user_by_id(user)
         go_back()
     
@@ -17,14 +49,20 @@ def show_profile(main_user, user):
             windows_init.show_consultant_window(main_user)
         elif main_user.type == "тьютор": 
             root.destroy()
-            windows_init.show_tutor_window(main_user)
+            windows_init.show_my_students(main_user)
         elif main_user.type == "студент" and main_user.competence == "диспетчер":
             root.destroy()
             windows_init.show_dispatcher_window(main_user)
         else:
-            root.destroy()
-            windows_init.show_admin_window(main_user)
-            
+            if user.type == "студент":
+                root.destroy()
+                windows_init.show_my_students(main_user)
+            elif user.type == "преподаватель":
+                root.destroy()
+                windows_init.show_my_teachers_window(main_user)
+            else:
+                root.destroy()
+                windows_init.show_admin_window(main_user)
 
     root = tk.Tk()
     root.resizable(False, False)
@@ -33,35 +71,72 @@ def show_profile(main_user, user):
     screen_height = root.winfo_screenheight() // 2 - 210 
     root.geometry('640x420+{}+{}'.format(screen_width, screen_height))
 
+    t_competence_list = ["гражданское право", "трудовое право", "административное право", "жилищное право"]
+    s_competence_list = ["консультант", "диспетчер"]
     db = DataBase()
 
+    label_name = tk.Label(text="Имя: ")
     if (main_user.type == "админ"):
         e_name = tk.Entry(width=40)
         e_name.insert(0, user.name)
     else:
-        l_name = tk.Label(width=40)
-        l_name['text'] =  'Имя: ' + user.name
+        l_name = tk.Label(text=user.name, width=40)
     
-    l_type = tk.Label(text="Тип: " + user.type)
-    l_comp = tk.Label(text="Компетенция: " + user.competence)
-    l_login = tk.Label(text="Логин: " + user.login)
-    l_points = tk.Label()
-    if user.type == "студент":
-        l_points['text'] = "Баллы: " + str(user.points)
+    label_type = tk.Label(text="Тип: ")
+    l_type = tk.Label(text=user.type)
+
+    label_comp = tk.Label(text="Компетенция: " + user.competence)
+    if main_user.type == "админ":
+        if user.type == "студент":
+            lb_comp = tk.Listbox(height=len(s_competence_list))
+            for r in s_competence_list:
+                lb_comp.insert(tk.END, r)
+        else:
+            lb_comp = tk.Listbox(height=len(t_competence_list))
+            for r in t_competence_list:
+                lb_comp.insert(tk.END, r)
+    else:
+        l_comp = tk.Label(text=user.competence)
+
+    label_login = tk.Label(text="Логин: ")
+    if main_user.type == "админ":
+        e_login  = tk.Entry(width=40)
+        e_login.insert(0, user.login)
+    else:
+        l_login = tk.Label(text=user.login)
+    
+    l_points = tk.Label(text="Баллы: " + str(user.points))
 
     b_back = tk.Button(text="Назад", command=go_back)
     b_save = tk.Button(text="Сохранить и выйти", command=save)
     
+    label_name.pack()
     if main_user.type == "админ": 
         e_name.pack()
     else:
         l_name.pack()
-
+    
+    label_type.pack()
     l_type.pack()
-    l_comp.pack()
-    l_login.pack()
-    l_points.pack()
+    
+    label_comp.pack()
+    if main_user.type == "админ":
+        lb_comp.pack()
+    else:
+        l_comp.pack()
+    
+    label_login.pack()
+    if main_user.type == "админ":
+        e_login.pack()
+    else:
+        l_login.pack()
+    
+    if user.type == "студент":
+        l_points.pack()
+    
     if main_user.type == "админ":
         b_save.pack(side="bottom")
+    
     b_back.pack(side=tk.RIGHT)
+
     root.mainloop()
