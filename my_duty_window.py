a1fb796b01ad542e9dbbd42f309fe504dee0dac3
alsvsl <alisa.vasilewa@yandex.ru>
2019-12-26 07:29:08 +0300

    edited my_duty_window.py

--- my_duty_window.py
+++ my_duty_window.py
@@ -5,6 +5,7 @@ from tkinter import messagebox as mb
 
 curr_duties = []
 
+
 def show_my_duties(main_user):
     def go_back():
         if main_user.type == "преподаватель":
@@ -16,13 +17,14 @@ def show_my_duties(main_user):
         else:
             root.destroy()
             windows_init.show_dispatcher_window(main_user)
+
     def show_duties():
-        lbox.delete(0,tk.END)
+        lbox.delete(0, tk.END)
         global curr_duties
         if var.get() == 0:
-            #curr_duties = db.get_ TODO: для конкретного студента
+            # curr_duties = db.get_ TODO: для конкретного студента
             curr_duties = db.get_prev_duties()
-        else: 
+        else:
             # TODO: для конкретного студента
             curr_duties = db.get_next_duties()
         if not curr_duties:
@@ -32,7 +34,7 @@ def show_my_duties(main_user):
         for dut in curr_duties:
             user = db.get_user_by_id(dut.s_id)
             lbox.insert(tk.END, user.name + " " + dut.date)
-    
+
     def add_duty():
         root.destroy()
         windows_init.show_add_duty(main_user)
@@ -41,19 +43,18 @@ def show_my_duties(main_user):
         global curr_duties
         select = list(lbox.curselection())
         if len(select) and curr_duties:
-            select_duty = curr_duties[ select[0] ]
+            select_duty = curr_duties[select[0]]
             root.destroy()
             windows_init.show_change_dispatcher_window(main_user, select_duty)
         else:
-            mb.showerror("Ошибка","Выберите дежурство!")
-
+            mb.showerror("Ошибка", "Выберите дежурство!")
 
     root = tk.Tk()
     root.resizable(False, False)
     root.title("Мои дежурства")
-    screen_width = root.winfo_screenwidth() // 2 - 320 
-    screen_height = root.winfo_screenheight() // 2 - 210 
-    root.geometry('640x420+{}+{}'.format(screen_width, screen_height))
+    screen_width = root.winfo_screenwidth() // 2 - 320
+    screen_height = root.winfo_screenheight() // 2 - 210
+    root.geometry('480x250+{}+{}'.format(screen_width, screen_height))
 
     db = DataBase()
 
@@ -62,21 +63,28 @@ def show_my_duties(main_user):
     r_prev = tk.Radiobutton(text="Прошедшие", variable=var, value=0)
     r_curr = tk.Radiobutton(text="Запланированные", variable=var, value=1)
 
-    b_show_duties = tk.Button(text="Показать", compound=tk.TOP, command=show_duties)
+    b_show_duties = tk.Button(
+        text="Показать", compound=tk.TOP, command=show_duties)
     b_change_der = tk.Button(text="Изменить дежурство", command=change_der)
     b_add_duty = tk.Button(text="Добавить дежурство", command=add_duty)
     b_back = tk.Button(text="Назад", command=go_back)
-    lbox = tk.Listbox(width = 40, height = 10)
-    label = tk.Label(width=40)
+    lbox = tk.Listbox(width=40, height=10)
 
-    r_curr.pack()
-    r_prev.pack()
-    b_show_duties.pack(side="top")
-    lbox.pack(side="top")
+    title = tk.Label()
+    title['text'] = "Список дежурств:"
+
+    title.place(x=0, y=0, width=300, height=20)
+    lbox.place(x=0, y=20, width=300, height=150)
+
+    r_curr.place(x=275, y=30, width=220, height=20)
+
+    r_prev.place(x=260, y=80,  width=220, height=20)
+
+    b_show_duties.place(x=335, y=120,  width=80, height=20)
+
     if main_user.type == "тьютор":
-        b_add_duty.pack()
-        b_change_der.pack()
-    label.pack()
-    b_back.pack(side=tk.RIGHT)
+        b_add_duty.place(x=0, y=170, width=300, height=30)
+        b_change_der.place(x=0, y=200, width=300, height=30)
+    b_back.place(x=0, y=230, width=40, height=20)
 
     root.mainloop()
