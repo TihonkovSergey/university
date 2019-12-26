2d9f2a94c8d39044971395f3413aaabc691fd3bc
alsvsl <alisa.vasilewa@yandex.ru>
2019-12-26 05:09:30 +0300

    edited my_points_window

--- my_points_window.py
+++ my_points_window.py
@@ -5,9 +5,10 @@ import windows_init
 
 curr_points = []
 
+
 def show_points(main_user):
     def refresh():
-        lbox.delete(0,tk.END)
+        lbox.delete(0, tk.END)
         global curr_points
         if main_user.type == "преподаватель":
             curr_points = db.get_points_event_by_t_id(main_user.id)
@@ -17,8 +18,10 @@ def show_points(main_user):
             for c in curr_points:
                 student = db.get_user_by_id(c.s_id)
                 if not student:
-                    student = User(("id", "неизвестный студент", "студент", "-", "-", "-", "", "0.0"))
-                lbox.insert(tk.END,c.points + " " + student.name + " " + c.reason + " " + c.date_time)
+                    student = User(("id", "неизвестный студент",
+                                    "студент", "-", "-", "-", "", "0.0"))
+                lbox.insert(tk.END, c.points + " " + student.name +
+                            " " + c.reason + " " + c.date_time)
         else:
             curr_points = db.get_points_event_by_s_id(main_user.id)
             if not curr_points:
@@ -27,8 +30,10 @@ def show_points(main_user):
             for c in curr_points:
                 teacher = db.get_user_by_id(c.t_id)
                 if not teacher:
-                    teacher = User(("id", "неизвестный преподаватель", "преподаватель", "-", "-", "-", "", "0.0"))
-                lbox.insert(tk.END,c.points + " " + teacher.name + " " + c.reason + " " + c.date_time)
+                    teacher = User(
+                        ("id", "неизвестный преподаватель", "преподаватель", "-", "-", "-", "", "0.0"))
+                lbox.insert(tk.END, c.points + " " + teacher.name +
+                            " " + c.reason + " " + c.date_time)
 
     def go_back():
         if main_user.type == "преподаватель":
@@ -43,25 +48,25 @@ def show_points(main_user):
         elif main_user.type == "студент" and main_user.competence == "диспетчер":
             root.destroy()
             windows_init.show_dispatcher_window(main_user)
-        else: # какая-то ошибка
+        else:  # какая-то ошибка
             root.destroy()
             windows_init.show_login(main_user)
 
     root = tk.Tk()
     root.resizable(False, False)
     root.title("История баллов")
-    screen_width = root.winfo_screenwidth() // 2 - 320 
-    screen_height = root.winfo_screenheight() // 2 - 210 
-    root.geometry('640x420+{}+{}'.format(screen_width, screen_height))
-    
+    screen_width = root.winfo_screenwidth() // 2 - 265
+    screen_height = root.winfo_screenheight() // 2 - 110
+    root.geometry('530x220+{}+{}'.format(screen_width, screen_height))
+
     db = DataBase()
     b_refresh = tk.Button(text="Обновить", command=refresh)
     b_back = tk.Button(text="Назад", command=go_back)
-    lbox = tk.Listbox(width = 80, height = 15)
+    lbox = tk.Listbox(width=80, height=10)
 
     lbox.pack()
     b_refresh.pack()
-    b_back.pack()
+    b_back.place(x=0, y=200, width=40, height=20)
     refresh()
 
     root.mainloop()
