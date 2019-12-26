72c2b45d6b46c7af44b1d5405f6dbf245c503e18
alsvsl <alisa.vasilewa@yandex.ru>
2019-12-26 05:50:24 +0300

    edited add_cons_teach_window.py

--- add_cons_teach_window.py
+++ add_cons_teach_window.py
@@ -6,11 +6,12 @@ from tkinter import messagebox as mb
 curr_teachers = []
 curr_students = []
 
+
 def add_cons_teach(main_user, case):
     def go_back():
         root.destroy()
         windows_init.show_case_window(main_user, case)
-    
+
     def show_teachers():
         global curr_teachers
         comp = case.category
@@ -28,16 +29,16 @@ def add_cons_teach(main_user, case):
             s_lbox.insert(tk.END, "Не найдено подходящих консультантов")
             curr_students = []
         for s in curr_students:
-            s_lbox.insert(tk.END, s.name + " " + str(s.points)) 
-    
+            s_lbox.insert(tk.END, s.name + " " + str(s.points))
+
     def ok():
         select_teachers = list(t_lbox.curselection())
         select_students = list(s_lbox.curselection())
-        if len(select_teachers) < 1:
+        if len(select_teachers) < 1 or not curr_teachers:
             mb.showerror("Ошибка", "Выберите куратора среди преподавателей")
             return
-        
-        if len(select_students) < 1:
+
+        if len(select_students) < 1 or not curr_students:
             mb.showerror("Ошибка", "Выберите консультанта")
             return
         case.status = "ожидаются правки плана консультации"
@@ -49,21 +50,27 @@ def add_cons_teach(main_user, case):
     root = tk.Tk()
     root.resizable(False, False)
     root.title("Добавление исполнителей")
-    screen_width = root.winfo_screenwidth() // 2 - 320 
-    screen_height = root.winfo_screenheight() // 2 - 320 
-    root.geometry('640x640+{}+{}'.format(screen_width, screen_height))
+    screen_width = root.winfo_screenwidth() // 2 - 210
+    screen_height = root.winfo_screenheight() // 2 - 135
+    root.geometry('420x270+{}+{}'.format(screen_width, screen_height))
 
     db = DataBase()
-    
+
     b_back = tk.Button(text="Назад", command=go_back)
-    t_lbox = tk.Listbox(exportselection=0, width = 40, height = 10)
-    s_lbox = tk.Listbox(exportselection=0, width = 40, height = 10)
+    t_lbox = tk.Listbox(exportselection=0, width=40, height=5)
+    s_lbox = tk.Listbox(exportselection=0, width=40, height=5)
     b_ok = tk.Button(text="OK", command=ok)
 
+    curator_label = tk.Label(text="Выберите куратора дела")
+    curator_label.pack()
     t_lbox.pack(side="top")
+
+    student_label = tk.Label(text="Выберите консультанта по делу")
+    student_label.pack()
     s_lbox.pack(side="top")
+
     b_ok.pack()
-    b_back.pack(side=tk.RIGHT)
+    b_back.place(x=0, y=250, width=40, height=20)
 
     show_teachers()
     show_student()
