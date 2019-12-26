8b7b47bf5dbdbc253eb9a0972208bd225e62e4c4
alsvsl <alisa.vasilewa@yandex.ru>
2019-12-26 06:34:29 +0300

    edited document_window.py

--- document_window.py
+++ document_window.py
@@ -5,10 +5,12 @@ from tkinter import messagebox as mb
 from tkinter import filedialog as fd
 from jcDocumentsClass import Document
 
+
 def show_document(main_user, case, doc):
     def go_back():
         root.destroy()
         windows_init.show_case_window(main_user, case)
+
     def save():
         t = text.get(0.0, tk.END)
         f = open(doc.document_id, "w")
@@ -19,13 +21,11 @@ def show_document(main_user, case, doc):
     root = tk.Tk()
     root.resizable(False, False)
     root.title("Документ " + doc.title)
-    screen_width = root.winfo_screenwidth() // 2 - 320 
-    screen_height = root.winfo_screenheight() // 2 - 320 
-    root.geometry('640x640+{}+{}'.format(screen_width, screen_height))
+    screen_width = root.winfo_screenwidth() // 2 - 320
+    screen_height = root.winfo_screenheight() // 2 - 210
+    root.geometry('640x400+{}+{}'.format(screen_width, screen_height))
 
-    db = DataBase()
-
-    text = tk.Text(width=50, height=25)
+    text = tk.Text(width=50, height=20)
     f = open(doc.document_id, "r")
     curr_text = f.read()
     f.close()
@@ -36,5 +36,5 @@ def show_document(main_user, case, doc):
 
     text.pack()
     b_save.pack()
-    b_back.pack()
+    b_back.place(x=0, y=380, width=40, height=20)
     root.mainloop()
