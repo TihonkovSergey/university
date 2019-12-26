93b072a6c74ebc9e8ca5b904abf9a5937954cb7c
alsvsl <alisa.vasilewa@yandex.ru>
2019-12-26 06:47:02 +0300

    edited add_one_user_window.py

--- add_one_user_window.py
+++ add_one_user_window.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from jcQueries import DataBase
 import windows_init
-from tkinter import filedialog as fd 
-from tkinter import messagebox as mb 
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from jcUserClass import User
 
 
@@ -10,18 +10,20 @@ def add_one_user(main_user):
     def go_back():
         root.destroy()
         windows_init.show_admin_window(main_user)
-    
+
     def add():
         user = User(("", "", "", "", "", "", "", "0.0"))
         name = e_name.get()
         if len(name) < 6:
-            mb.showerror("Ошибка", "Имя пользователя должно быть длиннее 5 символов!")
+            mb.showerror(
+                "Ошибка", "Имя пользователя должно быть длиннее 5 символов!")
             return
 
         for c in name:
-            if not (("a" <= c <= "z") or ("A" <= c <= "Z") or 
-                    ("а" <= c <= "я") or ("А" <= c <= "Я") or c ==" "):
-                mb.showerror("Ошибка", "Имя пользователя должно содержать только буквы!")
+            if not (("a" <= c <= "z") or ("A" <= c <= "Z") or
+                    ("а" <= c <= "я") or ("А" <= c <= "Я") or c == " "):
+                mb.showerror(
+                    "Ошибка", "Имя пользователя должно содержать только буквы!")
                 return
         user.name = name
 
@@ -34,10 +36,11 @@ def add_one_user(main_user):
         if len(login) < 4:
             mb.showerror("Ошибка", "Логин должен быть длиннее 4 символов!")
             return
-        
+
         for c in login:
             if not (("a" <= c <= "z") or ("A" <= c <= "Z") or ("0" <= c <= "9")):
-                mb.showerror("Ошибка", "Логин должен содержать только латинские буквы и цифры!")
+                mb.showerror(
+                    "Ошибка", "Логин должен содержать только латинские буквы и цифры!")
                 return
         user.login = login
 
@@ -55,7 +58,7 @@ def add_one_user(main_user):
 
         user.password = e_password.get()
         db.insert_users([user])
-        go_back() 
+        go_back()
 
     def change():
         lb_comp.delete(0, tk.END)
@@ -66,27 +69,32 @@ def add_one_user(main_user):
             for c in s_competence_list:
                 lb_comp.insert(tk.END, c)
 
-
     root = tk.Tk()
     root.resizable(False, False)
     root.title("Добавление нового пользователя")
-    
-    screen_width = root.winfo_screenwidth() // 2 - 320 
-    screen_height = root.winfo_screenheight() // 2 - 210 
-    root.geometry('640x420+{}+{}'.format(screen_width, screen_height))
+
+    screen_width = root.winfo_screenwidth() // 2 - 210
+    screen_height = root.winfo_screenheight() // 2 - 100
+    root.geometry('420x290+{}+{}'.format(screen_width, screen_height))
 
     s_competence_list = ["консультант", "диспетчер"]
-    t_competence_list = ["гражданское право", "трудовое право", "административное право", "жилищное право"]
+    t_competence_list = ["гражданское право", "трудовое право",
+                         "административное право", "жилищное право"]
 
     db = DataBase()
+
+    fio_label = tk.Label(text="ФИО")
+    fio_label.pack()
 
     e_name = tk.Entry(width=40)
     type_var = tk.IntVar()
     type_var.set(0)
-    r_s = tk.Radiobutton(text="Студент", variable=type_var, value=0, command=change)
-    r_t = tk.Radiobutton(text="Преподаватель", variable=type_var, value=1, command=change)
+    r_s = tk.Radiobutton(text="Студент", variable=type_var,
+                         value=0, command=change)
+    r_t = tk.Radiobutton(text="Преподаватель",
+                         variable=type_var, value=1, command=change)
 
-    lb_comp = tk.Listbox(width = 40, height = 4)
+    lb_comp = tk.Listbox(width=40, height=4)
     change()
     label_login = tk.Label(text="Логин:")
     e_login = tk.Entry(width=40)
@@ -97,7 +105,7 @@ def add_one_user(main_user):
     b_add = tk.Button(text="Добавить", command=add)
 
     e_name.pack()
-    
+
     r_s.pack()
     r_t.pack()
     lb_comp.pack()
@@ -105,7 +113,7 @@ def add_one_user(main_user):
     e_login.pack()
     label_password.pack()
     e_password.pack()
-    
+
     b_add.pack()
-    b_back.pack()
+    b_back.place(x=0, y=270, width=40, height=20)
     root.mainloop()
