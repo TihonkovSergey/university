8ded66ec208facc0e1d03fb90db893f09bd2e844
alsvsl <alisa.vasilewa@yandex.ru>
2019-12-26 08:08:08 +0300

    edited change_dispatcher_window.py

--- change_dispatcher_window.py
+++ change_dispatcher_window.py
@@ -7,6 +7,7 @@ from jcDutyClass import Duty
 
 curr_students = []
 
+
 def change_disp(main_user, duty):
     def go_back():
         root.destroy()
@@ -29,14 +30,14 @@ def change_disp(main_user, duty):
     root = tk.Tk()
     root.resizable(False, False)
     root.title("Изменение дежурства")
-    screen_width = root.winfo_screenwidth() // 2 - 320 
-    screen_height = root.winfo_screenheight() // 2 - 320 
-    root.geometry('640x640+{}+{}'.format(screen_width, screen_height))
+    screen_width = root.winfo_screenwidth() // 2 - 210
+    screen_height = root.winfo_screenheight() // 2 - 135
+    root.geometry('420x270+{}+{}'.format(screen_width, screen_height))
 
     db = DataBase()
     b_del = tk.Button(text="Удалить дежурство", command=delete)
     b_back = tk.Button(text="Назад", command=go_back)
-    t_lbox = tk.Listbox(exportselection=0, width = 40, height = 10)
+    t_lbox = tk.Listbox(exportselection=0, width=40, height=10)
 
     global curr_students
     curr_students = db.get_dispatchers()
@@ -48,11 +49,11 @@ def change_disp(main_user, duty):
             t_lbox.insert(tk.END, s.name)
 
     b_ok = tk.Button(text="OK", command=ok)
-    e_date = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2, year=2019)
+    e_date = DateEntry(root, width=12, background='darkblue',
+                       foreground='white', borderwidth=2, year=2019)
     e_date.pack(padx=10, pady=10)
     t_lbox.pack(side="top")
     b_ok.pack()
     b_del.pack()
-    b_back.pack(side=tk.RIGHT)
-
+    b_back.place(x=0, y=250, width=40, height=20)
     root.mainloop()
